7e8c70163700511801bf9a27d034f7ee5c560a56
amansanill <aman.a.sanil@gmail.com>
2025-03-12 10:28:18 +0000

    Update frontend

--- src/App.js
+++ src/App.js
@@ -6,35 +6,24 @@ import axios from 'axios';
 function App() {
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState('');
-  const [deployMessage, setDeployMessage] = useState('');
-
-  const backendUrl = 'http://13.126.86.158:5000';
 
   useEffect(() => {
-    axios.get(`${backendUrl}/tasks`).then((response) => {
+    axios.get('http://13.126.86.158:5000/tasks').then((response) => {
       setTasks(response.data);
     });
   }, []);
 
   const addTask = () => {
     const newTask = { id: Date.now().toString(), name: task };
-    axios.post(`${backendUrl}/tasks`, newTask).then(() => {
+    axios.post('http://13.126.86.158:5000/tasks', newTask).then(() => {
       setTasks([...tasks, newTask]);
       setTask('');
     });
   };
 
   const deleteTask = (id) => {
-    axios.delete(`${backendUrl}/tasks/${id}`).then(() => {
+    axios.delete(http://13.126.86.158:5000/tasks/${id}).then(() => {
       setTasks(tasks.filter((t) => t.id !== id));
-    });
-  };
-
-  const testDeployment = () => {
-    axios.get(`${backendUrl}/test-deploy`).then((response) => {
-      setDeployMessage(response.data);
-    }).catch(() => {
-      setDeployMessage('Failed to reach backend');
     });
   };
 
@@ -48,10 +37,6 @@ function App() {
         placeholder="Add a new task" 
       />
       <button onClick={addTask}>ADD TASK</button>
-      <button onClick={testDeployment} style={{ marginLeft: '10px' }}>
-        TEST BACKEND DEPLOYMENT
-      </button>
-      {deployMessage && <p>{deployMessage}</p>}
       <ul>
         {tasks.map((t) => (
           <li key={t.id}>
